7bb7f6cb853b4ba19b88a1d1e4cc6852cfe87eff
Daniela Correa <dacomu24@gmail.com>
2022-04-25 16:17:39 -0500

    I just add some final styling details

--- src/data.js
+++ src/data.js
@@ -28,5 +28,38 @@ export default [
         endDate: "24 Jan, 2021",
         description: "The Northern Lights. The extreme dark of the Icelandic winter has a few perks. Between September and April, people in Iceland are treated to a magnificent natural display: the phenomenon of aurora borealis, or what we commonly call the Northern Lights",
         imageUrl: "images/iceland.jpg"
-    }
+    },
+
+    {
+        id: 4,
+        title: "Iceland",
+        location: "Iceland",
+        googleMapsUrl: "https://goo.gl/maps/NTjWYsx9QiRneb1y5",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The Northern Lights. The extreme dark of the Icelandic winter has a few perks. Between September and April, people in Iceland are treated to a magnificent natural display: the phenomenon of aurora borealis, or what we commonly call the Northern Lights",
+        imageUrl: "images/iceland.jpg"
+    },
+    {
+        id: 5,
+        title: "Iceland",
+        location: "Iceland",
+        googleMapsUrl: "https://goo.gl/maps/NTjWYsx9QiRneb1y5",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The Northern Lights. The extreme dark of the Icelandic winter has a few perks. Between September and April, people in Iceland are treated to a magnificent natural display: the phenomenon of aurora borealis, or what we commonly call the Northern Lights",
+        imageUrl: "images/iceland.jpg"
+    },
+
+    {
+        id: 6,
+        title: "Sognefjord",
+        location: "Sweden",
+        googleMapsUrl: "https://goo.gl/maps/7S5BXjFw8etdMsVv8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The Sognefjord or Sognefjorden, nicknamed the King of the Fjords, is the largest and deepest fjord in Norway. Located in Vestland county in Western Norway, it stretches 205 kilometres inland from the ocean to the small village of Skjolden in the municipality of Luster",
+        imageUrl: "images/Sognefjord.jpg"
+        }
+
 ]
